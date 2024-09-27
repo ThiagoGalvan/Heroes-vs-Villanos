174e9729d0489af437c2fd0d5a03d60bec4e8323
gian-marchisio <gmarchisio@etrr.edu.ar>
2024-09-27 08:48:57 -0300

    Update personajes.c

--- personajes.c
+++ personajes.c
@@ -16,6 +16,8 @@ typedef struct{
 
 //Batman
 personaje_t batman = {"Batman", 100, 100, 100, 100, 100, 100, 100, 100,"DC"};
+//Joker
+personaje_t joker = {"Joker", 100, 100, 100, 100, 100, 100, 100, 100, "DC"};
 int main(){
     printf("Nombre:%s\n",batman.nombre);
     printf("Resistencia:%d\n",batman.resistencia);
@@ -27,4 +29,16 @@ int main(){
     printf("Edad:%d\n",batman.edad);
     printf("IQ:%d\n",batman.iq);
     printf("Franquicia:%s\n",batman.franquicia);
+
+    // Mostrar habilidades de Joker
+    printf("Nombre: %s\n", joker.nombre);
+    printf("Resistencia: %d\n", joker.resistencia);
+    printf("Resistencia al fuego: %d\n", joker.resistencia_fuego);
+    printf("Resistencia eléctrica: %d\n", joker.resistencia_elec);
+    printf("Vida: %d\n", joker.vida);
+    printf("Fuerza: %d\n", joker.fuerza);
+    printf("Velocidad: %d\n", joker.velocidad);
+    printf("Edad: %d\n", joker.edad);
+    printf("IQ: %d\n", joker.iq);
+    printf("Franquicia: %s\n", joker.franquicia);
 }
