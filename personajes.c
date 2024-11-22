a9218f6f8ae97bb83febbc5beafd7bb9ac18f922
JuaniAbila <168846553+JuaniAbila@users.noreply.github.com>
2024-11-22 14:58:51 -0300

    Update personajes.c
    chimblandung

--- personajes.c
+++ personajes.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define MAX_CHAR 50
-#define MAX_ATACKS 3
+#define MAX_ATTACKS 3
 
 typedef struct {
    char nombre[MAX_CHAR];
@@ -20,12 +21,11 @@ typedef struct {
 
 typedef struct {
    char nombre[MAX_CHAR];
-   ataque_t ataques[MAX_ATACKS];
-   defensa_t defensas[MAX_ATACKS];
+   ataque_t ataques[MAX_ATTACKS];
+   defensa_t defensas[MAX_ATTACKS];
    int vida;
    int vida_maxima;
    int velocidad;
-   int evasion;
 } personaje_t;
 
 // Prototipos de funciones
@@ -37,18 +37,43 @@ void elegirAtaque(personaje_t *pj, personaje_t *oponente);
 // Definición de personajes
 const personaje_t batman = {
    "Batman",
-   {{"Batarang", 20, 5}, {"Golpe", 15, 5}, {"Gadget", 25, 3}},
+   {{"Batarang", 10, 10}, {"Batarang Explosivo", 25, 5}, {"Batipatada", 15, 5}},
    {{"Curación", 10, 0, 5}},
-   100, 100, 10, 5};
+   120, 120, 15};
 
 const personaje_t joker = {
    "Joker",
-   {{"Risa Tóxica", 18, 5}, {"Bomba de Nafta", 22, 4}, {"Truco", 15, 5}},
+   {{"Gas De La Risa", 25, 3}, {"Revolver Largo", 10, 8}, {"Golpe De Secuaz", 15, 5}},
    {{"Curación", 10, 0, 5}},
-   100, 100, 8, 3};
+   90, 90, 15};
 
+const personaje_t flecha = {
+   "Flecha",
+   {{"Disparo", 12, 5}, {"Arco", 20,3},{"Patada Medialuna", 10, 5}},
+   {{"Curación", 10, 0, 5}},
+   120, 120, 12};
+
+const personaje_t john = {
+   "John Constantine",
+   {{"Hechizo De Fuego", 25, 5}, {"Invocacion Satanica", 30, 3},{"Cadenas Diabolicas", 20, 10}},
+   {{"Curación", 10, 0, 5}},
+   70, 70, 8};
+
+
+const personaje_t deathstroke = {
+   "Deathstroke",
+   {{"Golpe De Katana", 20, 5}, {"Guillotina", 45, 1}, {"Disparo", 10, 8}},
+   {{"Curación", 10, 0, 5}},
+   150, 150, 5};
+
+const personaje_t robin = {
+   "Robin",
+   {{"Estocada De Espada", 15, 3}, {"Corte Profundo", 20, 5}, {"Patada Giratoria", 10, 5}}
+   {{"Curación", 10, 0, 5}}
+   
+}
 int main() {
-   srand(time(NULL)); // Inicializar la semilla para rand
+   srand(time(NULL));
    int eleccion = Bienvenida();
    personaje_t pj1, pj2;
 
@@ -58,9 +83,6 @@ int main() {
    } else if (eleccion == 2) {
       pj1 = joker;
       pj2 = batman;
-   } else {
-      printf("Opción no válida. Saliendo...\n");
-      return 1;
    }
    mostrarStats(&pj1);
    mostrarStats(&pj2);
@@ -76,6 +98,17 @@ int Bienvenida(void) {
    printf("Elija un personaje:\n");
    printf("1 - Batman\n");
    printf("2 - Joker\n");
+   printf("3 - Flecha\n");
+   printf("4 - John Constantine\n");
+   printf("5 - Deathstroke\n");
+   printf("6 - Robin\n");
+   printf("7 - Red Hood\n");
+   printf("8 - Nightwing\n");
+   printf("9 - Damian Wayne\n");
+   printf("10 - Azrael\n");
+   printf("11 - Bane\n");
+   printf("12 - Ra's al Ghul\n");
+
    scanf("%d", &eleccion);
 
    if (eleccion >= 1 && eleccion <= 2) {
@@ -93,13 +126,14 @@ void mostrarStats(const personaje_t *pj) {
    printf("Velocidad: %d\n", pj->velocidad);
    printf("Evasión: %d%%\n", pj->evasion);
    printf("Ataques:\n");
-   for (int i = 0; i < MAX_ATACKS; i++) {
+   for (int i = 0; i < MAX_ATTACKS; i++) {
       printf("  %d. %s (Potencia: %d, Usos: %d)\n",
          i + 1, pj->ataques[i].nombre, pj->ataques[i].potencia, pj->ataques[i].usos);
    }
 }
 
 void combate(personaje_t *pj1, personaje_t *pj2) {
+   int JokerDown;
    printf("\nComienza el combate entre %s y %s!\n", pj1->nombre, pj2->nombre);
    while (pj1->vida > 0 && pj2->vida > 0) {
       if (pj1->velocidad >= pj2->velocidad) {
@@ -116,13 +150,14 @@ void combate(personaje_t *pj1, personaje_t *pj2) {
                break;
             }
             elegirAtaque(pj1, pj2);
+            
       }
    }
 }
 
 void elegirAtaque(personaje_t *pj, personaje_t *oponente) {
    printf("\n%s, elige un ataque:\n", pj->nombre);
-   for (int i = 0; i < MAX_ATACKS; i++) {
+   for (int i = 0; i < MAX_ATTACKS; i++) {
       printf("%d. %s (Potencia: %d, Usos: %d)\n",
          i + 1, pj->ataques[i].nombre, pj->ataques[i].potencia, pj->ataques[i].usos);
    }
@@ -130,7 +165,7 @@ void elegirAtaque(personaje_t *pj, personaje_t *oponente) {
    int eleccion;
    scanf("%d", &eleccion);
 
-   if (eleccion < 1 || eleccion > MAX_ATACKS || pj->ataques[eleccion - 1].usos <= 0) {
+   if (eleccion < 1 || eleccion > MAX_ATTACKS || pj->ataques[eleccion - 1].usos <= 0) {
       printf("Opción inválida o ataque sin usos restantes. Turno perdido.\n");
       return;
    }
